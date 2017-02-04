5fd28f6dcd57f98afd1d3ab1a809c0a33f620844
Georgios Trichopoulos <gtricho@gmail.com>
2017-02-04 13:04:34 +0200

    Add files via upload

---
+++ warning.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="fall.css">
+
+<title>Εξομοίωση πτώσης - Προσοχή!</title>
+</head>
+<body>
+<div id="title">
+Προσομοίωση πτώσης και ειδοποίησης
+</div>
+
+<div id="subtitle">
+Τριχόπουλος Γεώργιος
+<img src="img/SmartHealth.png" alt="Logo">
+</div>
+
+<div id="Warning2">
+ΠΡΟΣΟΧΗ! Πιθανή πτώση!
+</div>
+
+<div id="footer">
+Εργασία - παρουσίαση στα πλαίσια του μαθήματος "Διάχυτη Υπολογιστική" - Διδάσκων: Καρυδάκης Γεώργιος - Πανεπιστήμιο Αιγαίου - 2017
+
+</div>
+</body>
+</html>
