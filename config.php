dc4866d4b53bae9f4de7588d890bf6cf942eb8a4
Georgios Trichopoulos <gtricho@gmail.com>
2017-02-04 12:59:39 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$c_db="ekpthem477031_Fall";
+$c_server="db24.grserver.gr:3306";
+$c_log="ekpth_Fall";
+$c_pass="Arn@k1Aspr0";
+$conn=mysqli_connect($c_server, $c_log, $c_pass, $c_db) or die("could not connect");
+?>
